b3e63063e3c2be2f68a7de0085f5935b7537c914
Joseph T. French <joey@harbinger.finance>
2026-01-30 22:14:15 -0600

    chore: remove ESLint configuration and ignore files, update Next.js and dependencies
    - Deleted .eslintignore and .eslintrc.cjs files to remove ESLint configuration.
    - Updated next.config.js to use ES module syntax.
    - Updated package.json and package-lock.json to reflect new versions for Next.js and other dependencies.
    - Adjusted tsconfig.json to change JSX setting and include additional type definitions.
    - Removed middleware.ts file and made minor adjustments in EntitySelectorDropdown component.

--- vitest.d.ts
+++ vitest.d.ts
@@ -3,8 +3,7 @@ import 'vitest'
 import type { vi } from 'vitest'
 
 declare global {
-  // eslint-disable-next-line no-var
   var jest: typeof vi
-  // eslint-disable-next-line no-var
+
   var mockFetch: (data: unknown) => void
 }
